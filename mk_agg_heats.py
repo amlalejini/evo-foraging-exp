c213795cc889d49419b500aabb9015bfa4b502c8
amlalejini <amlalejini@gmail.com>
2016-04-21 18:06:05 -0400

    Cleaned up run_pca script. Added some stuff to mk_agg_heats script

--- mk_agg_heats.py
+++ mk_agg_heats.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
         settings = json.load(fp)
     metrics_dump = settings["analysis"]["metrics_dump"]
     world_width = settings["analysis"]["world_width"]
+    test_envs_loc = settings["analysis"]["test_envs_loc"]
 
     paths_fpath = os.path.join(metrics_dump, "paths.csv")
     paths = None
@@ -17,8 +18,15 @@ if __name__ == "__main__":
     paths = paths[1:len(paths)]
     # Create a column lookup dictionary (given data name, give column)
     column_idxs = {headers[i]: i for i in range(0, len(headers))}
+    ####################################################################################################
+    ####################################################################################################
+    ####################################################################################################
+
+    ####################################################################################################
+    ####################################################################################################
+    ####################################################################################################
     ###############################################
-    # Make aggregated heat maps for each treatment (aggregate accross environments)
+    # Make aggregated heat maps for each treatment (just home)
     agg_maps = {}
     for path in paths:
         # Extract relevant data:
@@ -28,7 +36,7 @@ if __name__ == "__main__":
         path_x = ast.literal_eval(path[column_idxs["x_path"]].replace("-1,", ""))
         path_y = ast.literal_eval(path[column_idxs["y_path"]].replace("-1,", ""))
         if path_env.replace("env__", "") != path_treatment.replace("_U-50000", ""): continue
-        if len(set(path_x)) == 1 and len(set(path_y)) == 1: continue
+        #if len(set(path_x)) == 1 and len(set(path_y)) == 1: continue
         # Have we processed this treatment before?
         if not path_treatment in agg_maps:
             # Add treatment to dictionary
@@ -55,22 +63,20 @@ if __name__ == "__main__":
     #     for k in agg_maps[t]:
     #         print "%s.%s: %f" % (t, k, sum(map(sum, agg_maps[t][k])))
     ###############################################
-    # Flatten aggretaged heatmaps
-    flat_agg_maps = {}
-    for t in agg_maps:
-        flat_agg_maps[t] = {}
-        for r in agg_maps[t]:
-            flat_agg_maps[t][r] = [(x / 87.0, y / 87.0, agg_maps[t][r][y][x]) for y in range(0, len(agg_maps[t][r])) for x in range(0, len(agg_maps[t][r][y]))]
-    ###############################################
-    # Write out aggregated heatmaps to file
-    csv_content = "treatment,rep,map\n"
-    for t in flat_agg_maps:
-        for r in flat_agg_maps[t]:
-            csv_content += "%s,%s,\"%s\"\n" % (t, r, str(flat_agg_maps[t][r]).replace(" ", ""))
-    with open(os.path.join(metrics_dump, "agg_maps.csv"), "w") as fp:
-        fp.write(csv_content)
-
-
+    # # Flatten aggretaged heatmaps
+    # flat_agg_maps = {}
+    # for t in agg_maps:
+    #     flat_agg_maps[t] = {}
+    #     for r in agg_maps[t]:
+    #         flat_agg_maps[t][r] = [(x / 87.0, y / 87.0, agg_maps[t][r][y][x]) for y in range(0, len(agg_maps[t][r])) for x in range(0, len(agg_maps[t][r][y]))]
+    # ###############################################
+    # # Write out aggregated heatmaps to file
+    # csv_content = "treatment,rep,map\n"
+    # for t in flat_agg_maps:
+    #     for r in flat_agg_maps[t]:
+    #         csv_content += "%s,%s,\"%s\"\n" % (t, r, str(flat_agg_maps[t][r]).replace(" ", ""))
+    # with open(os.path.join(metrics_dump, "agg_maps.csv"), "w") as fp:
+    #     fp.write(csv_content)
     ###############################################
     # Flatten aggretaged heatmaps and toss the x y values
     flat_agg_maps = {}
@@ -84,7 +90,54 @@ if __name__ == "__main__":
     for t in flat_agg_maps:
         for r in flat_agg_maps[t]:
             csv_content += "%s,%s,\"%s\"\n" % (t, r, str(flat_agg_maps[t][r]).replace(" ", ""))
-    with open(os.path.join(metrics_dump, "agg_maps2.csv"), "w") as fp:
+    with open(os.path.join(metrics_dump, "agg_maps_homes.csv"), "w") as fp:
         fp.write(csv_content)
+    # try with no g-16-p0
+    csv_content = "treatment,rep,map\n"
+    for t in flat_agg_maps:
+        if t == "T-256_G-16_A-64_P-0_U-50000":
+            print "Leave out this one!"
+            continue
+        for r in flat_agg_maps[t]:
+            csv_content += "%s,%s,\"%s\"\n" % (t, r, str(flat_agg_maps[t][r]).replace(" ", ""))
+    with open(os.path.join(metrics_dump, "agg_maps_homes-NOG16P0.csv"), "w") as fp:
+        fp.write(csv_content)
+    ####################################################################################################
+    ####################################################################################################
+    ####################################################################################################
 
+    ###############################################
+    # Make heat map for each environment
+    test_envs_maps = {}
+    for path in paths:
+        # Extract relevant data:
+        path_treatment = path[column_idxs["treatment"]]
+        path_rep = path[column_idxs["rep"]]
+        path_env = path[column_idxs["env"]]
+        path_x = ast.literal_eval(path[column_idxs["x_path"]].replace("-1,", ""))
+        path_y = ast.literal_eval(path[column_idxs["y_path"]].replace("-1,", ""))
+        # have we seen something like this before?
+        if not path_env in test_envs_maps:
+            test_envs_maps[path_env] = {}
+        if not path_treatment in test_envs_maps[path_env]:
+            test_envs_maps[path_env][path_treatment] = {}
+        if not path_rep in test_envs_maps[path_env][path_treatment]:
+            test_envs_maps[path_env][path_treatment][path_rep] = [[0 for _ in range(0, world_width)] for _ in range(0, world_width)]
+        # Add path to map
+        for i in range(0, len(path_x)):
+            test_envs_maps[path_env][path_treatment][path_rep][path_y[i]][path_x[i]] += 1
+        this_map = test_envs_maps[path_env][path_treatment][path_rep]
+        # Flatten
+        flat = [this_map[y][x] for y in range(0, len(this_map)) for x in range(0, len(this_map[y]))]
+        test_envs_maps[path_env][path_treatment][path_rep] = flat
+
+    ###############################################
+    # Write out aggregated heatmaps to file
+    for env in test_envs_maps:
+        csv_content = "treatment,rep,env,map\n"
+        for t in test_envs_maps[env]:
+            for r in test_envs_maps[env][t]:
+                csv_content += "%s,%s,%s,\"%s\"\n" % (t, r, env, str(test_envs_maps[env][t][r]).replace(" ", ""))
+        with open(os.path.join(metrics_dump, "agg_maps_%s.csv" % env), "w") as fp:
+            fp.write(csv_content)
     print "DONE"
--- run_pca.py
+++ run_pca.py
@@ -1,8 +1,16 @@
-import json, os, csv, ast, sys, math
+import json, os, csv, ast, sys, math, re
 import numpy as np
 import matplotlib.pyplot as plt
 from operator import itemgetter
 from cluster import *
+
+##########################################
+# TODO:
+#  * Clean this code up
+#  * variablize this stuff (get rid of magic numbers)
+#  * calculate % variance
+#  * visualize clusters (using top 2 thingies)
+
 
 def distanceMetric(a, b):
     # euc distance between a and b
@@ -20,72 +28,98 @@ def calcCentroid(bucket):
 
 if __name__ == "__main__":
     settings_fp = "param/settings.json"
+    FORCE_GEN_EIGEN_VECTORS = False
     # Load up the settings
     with open(settings_fp) as fp:
         settings = json.load(fp)
     metrics_dump = settings["analysis"]["metrics_dump"]
     world_width = settings["analysis"]["world_width"]
 
-    agg_maps_path = os.path.join(metrics_dump, "agg_maps2.csv")
+    # Load the aggregate heat maps (each map is a data point to be clustered)
+    agg_maps_fn = "agg_maps_homes-NOG16P0.csv"
+    agg_maps_path = os.path.join(metrics_dump, agg_maps_fn)
     csv.field_size_limit(sys.maxsize)
     with open(agg_maps_path) as fp:
          reader = csv.reader(fp, delimiter = ",", quotechar = '"')
          maps = [row for row in reader]
+    # Grab 'dem headers
     headers = maps[0]
+    # Grab 'dem maps
     maps = maps[1:len(maps)]
     # Create a column lookup dictionary (given data name, give column)
     column_idxs = {headers[i]: i for i in range(0, len(headers))}
 
-    labels = []
-    all_samples = []
+    labels = []         # labels is list of labels (synchronized with all_samples) for each sample
+    all_samples = []    # all_samples contains list of maps (data points)
     for m in maps:
         all_samples.append(ast.literal_eval(m[column_idxs["map"]]))
         labels.append(m[column_idxs["treatment"]] + "." + m[column_idxs["rep"]])
-
+    # Turn labels and all_samples into numpy arrays (to do crazy pca math on)
+    #  Not really necessary to do the same with labels (math is only done on all-samples), but I want to convince myself that labels and all_samples stay synchronized
     all_samples = np.asarray(all_samples)
-    all_samples = np.transpose(all_samples)
+    all_samples = np.transpose(all_samples) # This is the matrix we'll use for PCA
     labels = np.asarray(labels)
     labels = np.transpose(labels)
+    full_num_dimensions = all_samples.shape[0]  # Grab the dimensionality of space we're working with
 
-    # Now we have correct matrix for pca
+    # Calculate the means of each column (basically, the mean of each variable)
     means = []
     for i in range(0, all_samples.shape[0]):
         means.append([np.mean(all_samples[i,:])])
     mean_vector = np.asarray(means)
-
-    # scatter_matrix = np.zeros((7744, 7744))
-    # for i in range(all_samples.shape[1]):
-    #     scatter_matrix += (all_samples[:,i].reshape(7744,1) - mean_vector).dot((all_samples[:,i].reshape(7744,1) - mean_vector).T)
-    # print('Scatter Matrix:\n', scatter_matrix)
-    # # eigenvectors and eigenvalues for the from the scatter matrix
-    # eig_val_sc, eig_vec_sc = np.linalg.eig(scatter_matrix)
-    # for i in range(len(eig_val_sc)):
-    #     eigvec_sc = eig_vec_sc[:,i].reshape(1,7744).T
-    #     print('Eigenvector {}: \n{}'.format(i+1, eigvec_sc))
-    #     print('Eigenvalue {} from scatter matrix: {}'.format(i+1, eig_val_sc[i]))
-    # np.save("eig_val_sc.npy", eig_val_sc)
-    # np.save("eig_vec_sc.npy", eig_vec_sc)
-
-    eig_val_sc = np.load("eig_val_sc.npy")
-    eig_vec_sc = np.load("eig_vec_sc.npy")
+    # Calculate the eigen vectors (only do this if we have to... it takes a non-insignificant amount of time)
+    if FORCE_GEN_EIGEN_VECTORS:
+        scatter_matrix = np.zeros((full_num_dimensions, full_num_dimensions))
+        for i in range(all_samples.shape[1]):
+            scatter_matrix += (all_samples[:,i].reshape(full_num_dimensions, 1) - mean_vector).dot((all_samples[:,i].reshape(full_num_dimensions, 1) - mean_vector).T)
+        print('Scatter Matrix:\n', scatter_matrix)
+        # eigenvectors and eigenvalues for the from the scatter matrix
+        eig_val_sc, eig_vec_sc = np.linalg.eig(scatter_matrix)
+        for i in range(len(eig_val_sc)):
+            eigvec_sc = eig_vec_sc[:,i].reshape(1, full_num_dimensions).T
+            print('Eigenvector {}: \n{}'.format(i+1, eigvec_sc))
+            print('Eigenvalue {} from scatter matrix: {}'.format(i+1, eig_val_sc[i]))
+        np.save(agg_maps_fn.replace(".csv", "") + "__eig_val_sc.npy", eig_val_sc)
+        np.save(agg_maps_fn.replace(".csv", "") + "__eig_vec_sc.npy", eig_vec_sc)
+    else:
+        try:
+            eig_val_sc = np.load(agg_maps_fn.replace(".csv", "") + "__eig_val_sc.npy")
+            eig_vec_sc = np.load(agg_maps_fn.replace(".csv", "") + "__eig_vec_sc.npy")
+        except:
+            scatter_matrix = np.zeros((full_num_dimensions, full_num_dimensions))
+            for i in range(all_samples.shape[1]):
+                scatter_matrix += (all_samples[:,i].reshape(full_num_dimensions, 1) - mean_vector).dot((all_samples[:,i].reshape(full_num_dimensions, 1) - mean_vector).T)
+            print('Scatter Matrix:\n', scatter_matrix)
+            # eigenvectors and eigenvalues for the from the scatter matrix
+            eig_val_sc, eig_vec_sc = np.linalg.eig(scatter_matrix)
+            for i in range(len(eig_val_sc)):
+                eigvec_sc = eig_vec_sc[:,i].reshape(1, full_num_dimensions).T
+                print('Eigenvector {}: \n{}'.format(i+1, eigvec_sc))
+                print('Eigenvalue {} from scatter matrix: {}'.format(i+1, eig_val_sc[i]))
+            np.save(agg_maps_fn.replace(".csv", "") + "__eig_val_sc.npy", eig_val_sc)
+            np.save(agg_maps_fn.replace(".csv", "") + "__eig_vec_sc.npy", eig_vec_sc)
 
     # Make a list of (eigenvalue, eigenvector) tuples
     eig_pairs = [(np.abs(eig_val_sc[i]), eig_vec_sc[:,i]) for i in range(len(eig_val_sc))]
+    # Get the sum of all of the eigen values (will help us calculated % variance explained)
+    eig_vals_sum = sum([np.abs(eig_val_sc[i]) for i in range(len(eig_val_sc))])
     # Sort the (eigenvalue, eigenvector) tuples from high to low
     eig_pairs.sort(key = itemgetter(0), reverse = True)
-    # for i in range(0, len(eig_pairs)):
-    #     print "%d: %s" % (i, str(eig_pairs[i][0]))
-        #if eig_pairs[i][0] < 10: break
-    #matrix_w = np.hstack(eig_pairs[0:46][1])
+    # print everything out
+    for i in range(0, len(eig_pairs)):
+        percent_var_explained = eig_pairs[i][0] / float(eig_vals_sum)
+        if percent_var_explained < 0.001: break
+        print "%d: %s (%s)" % (i, str(eig_pairs[i][0]), str(percent_var_explained))
+
+    # How many dimensions from PCA are we going to use?
+    dimensions_to_use = 37
     listy = []
-    for i in range(0, 30):
-        listy.append(np.asarray(eig_pairs[i][1]).reshape(7744,1))
+    for i in range(0, dimensions_to_use):
+        listy.append(np.asarray(eig_pairs[i][1]).reshape(full_num_dimensions, 1))
     tupy = tuple(listy)
     matrix_w = np.hstack(tupy)
-    #print tuple(eig_pairs[0:46][1])
-    #matrix_w = np.hstack(( eig_pairs[0][1].reshape(7744,1), eig_pairs[1][1].reshape(7744,1), eig_pairs[2][1].reshape(7744,1), eig_pairs[3][1].reshape(7744,1), eig_pairs[4][1].reshape(7744,1), eig_pairs[5][1].reshape(7744,1), eig_pairs[6][1].reshape(7744,1) ))
+    # Transform our data according to PCA stuff
     transformed = matrix_w.T.dot(all_samples)
-
     # Turn pca data into data for clustering
     cluster_data = []
     for ri in range(0, len(labels)):
@@ -97,16 +131,33 @@ if __name__ == "__main__":
 
     # Cluster stuff
     kcluster = KMeansCluster(distanceMetric = distanceMetric, calculateCentroid = calcCentroid, data = cluster_data, force_k_bins = True)
-    results = kcluster.run(num_bins = 8)
+    results = kcluster.run(num_bins = 5)
     # Report clustered things (treatment.rep)
     for cluster in results["cluster_results"]:
         print "CLUSTER: " + str(cluster["bin_id"])
         for thing in cluster["data"]:
             print "    %s" % (thing["label"])
 
-    plt.plot(transformed[0,0:60], transformed[1,0:60], 'o', markersize=7, color='blue', alpha=0.5, label='class1')
-    plt.xlabel('x_values')
-    plt.ylabel('y_values')
-    plt.legend()
-    plt.title('Transformed samples with class labels')
+    # plt.plot(transformed[0,0:60], transformed[1,0:60], 'o', markersize=7, color='blue', alpha=0.5, label='class1')
+    # plt.xlabel('x_values')
+    # plt.ylabel('y_values')
+    # plt.legend()
+    # plt.title('Transformed samples with class labels')
+    # plt.show()
+
+    # Plot clusters
+    # results: {"cluster_results": [{"centroid": {"label": "", "data": []}, "data": [{"label": "", "data": []}]}, {}]}
+    treatment_shapes = {"T-256_G-1_A-64_P-0": "o", "T-256_G-1_A-64_P-1.0": "s","T-256_G-2_A-64_P-0": "D", "T-256_G-2_A-64_P-1.0": "*", "T-256_G-16_A-64_P-0": "^", "T-256_G-16_A-64_P-1.0": "+"}
+    cluster_colors = ["r", "b", "y", "g", "m", "c"]
+    plt.hold(True)
+    for ci in range(0, len(results["cluster_results"])):
+        this_cluster = results["cluster_results"][ci]
+        # Plot centroid
+        centroid_x = this_cluster["centroid"]["data"][0]
+        centroid_y = this_cluster["centroid"]["data"][1]
+        plt.plot([centroid_x], [centroid_y], "x", color = cluster_colors[ci % len(cluster_colors)], alpha = 1.0)
+        for point in this_cluster["data"]:
+            pltlabel = point["label"].split(".rep")[0].replace("_U-50000", "")
+            plt.plot([point["data"][0]], [point["data"][1]], marker = treatment_shapes[pltlabel], color = cluster_colors[ci % len(cluster_colors)], alpha = 0.5)
+
     plt.show()
